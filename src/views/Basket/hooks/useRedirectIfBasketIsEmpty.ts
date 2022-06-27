4954b810d358dd0906174f37eef19b50fcc68c32
zajdawid <zajdawid@protonmail.com>
2022-06-27 22:22:04 +0200

    fix: redirect hook

--- src/views/Basket/hooks/useRedirectIfBasketIsEmpty.ts
+++ src/views/Basket/hooks/useRedirectIfBasketIsEmpty.ts
@@ -1,16 +1,21 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useBasketHelper } from 'hooks';
 import { appPaths } from 'routes';
 
 const useRedirectIfBasketIsEmpty = () => {
   const navigate = useNavigate();
+  const isMountedRef = useRef<boolean>(true);
   const { basketItemsQty } = useBasketHelper();
 
   useEffect(() => {
-    if (!basketItemsQty) {
+    if (isMountedRef.current && !basketItemsQty) {
       navigate(appPaths.root());
     }
+
+    return () => {
+      isMountedRef.current = false;
+    };
   }, [basketItemsQty]);
 };
 
